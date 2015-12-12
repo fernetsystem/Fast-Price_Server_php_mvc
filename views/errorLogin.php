7a189793fa26074595beeab829305934fc7ede92
Jth Rdz <fernetsystem@gmail.com>
2015-12-12 01:17:09 -0600

    view error login

---
+++ views/errorLogin.php
@@ -0,0 +1,9 @@
+<br/>
+<div class="container">
+	<div class="panel panel-danger">
+  		<div class="panel-heading">ERROR DE AUTENTICACIÓN</div>
+  		<div class="panel-body">
+    		<h3>El e-mail o password  que ingreso son incorrectos...</h3>
+		</div>
+	</div>
+</div>
