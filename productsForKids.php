596b606fe9e1d9e97a4bd617a8627596a6c80b83
Jth Rdz <fernetsystem@gmail.com>
2015-12-12 01:11:16 -0600

    return json all products for kids

---
+++ productsForKids.php
@@ -0,0 +1,24 @@
+<?php  
+
+	$conexion = mysqli_connect('localhost','u858464654_fer','w9w9dorotea','u858464654_bdrem');
+
+$q = "select nombre_pro,descripcion,contenido,existencia,precio,nombre_suc,direccion from productos as p inner join sucursales as s on p.idsucursal = s.idsucursal where categoria='BEBES' and est_pago='PAGADO' order by nombre_pro";
+
+	$Mdatos = array();
+
+	$resultado = mysqli_query($conexion,$q);
+
+	while ($fila = mysqli_fetch_array($resultado,MYSQLI_ASSOC)) {
+
+		$Mdatos[] = $fila;
+
+	}
+
+	mysqli_close($conexion);
+
+	
+	echo json_encode($Mdatos);
+
+#	echo "<pre><code>".json_encode($Mdatos, JSON_PRETTY_PRINT)."</code></pre>";
+
+	?>
