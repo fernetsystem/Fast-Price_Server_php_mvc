b6ee15b78f56f79e5fdf54ec716c4a467f14790e
Jth Rdz <fernetsystem@gmail.com>
2015-12-12 01:10:40 -0600

    return for  post a json

---
+++ search_pro.php
@@ -0,0 +1,23 @@
+<?php 
+$word = $_POST['word'];
+#$word = "met";
+
+
+$conexion = mysqli_connect('localhost','u858464654_fer','w9w9dorotea','u858464654_bdrem');
+
+$q = "select nombre_pro,descripcion,contenido,existencia,precio,nombre_suc,direccion from productos as p inner join sucursales as s on p.idsucursal = s.idsucursal where nombre_pro like'$word%' or descripcion like '$word%' and est_pago='PAGADO' order by nombre_pro";
+$Mdatos = array();
+
+$resultado = mysqli_query($conexion,$q);
+
+while ($fila = mysqli_fetch_array($resultado,MYSQLI_ASSOC)) {
+		$Mdatos[] = $fila;
+}
+
+mysqli_close($conexion);
+
+echo json_encode($Mdatos);
+#echo "<pre><code>".json_encode($Mdatos, JSON_PRETTY_PRINT)."</code></pre>";
+
+?>
+
