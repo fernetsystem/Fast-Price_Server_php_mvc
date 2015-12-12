9d1b9e11d1db6afc3b17fadb4f9b628d4e4f8655
Jth Rdz <fernetsystem@gmail.com>
2015-12-12 01:11:51 -0600

    init MVC on index

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once("model/db.php");
+require_once("controller/controller.php");
+
+
+// Todo esta lógica hara el papel de un FrontController
+if(!isset($_REQUEST['c']))
+{
+    $controller = new controller;
+    $controller->Index();    
+} else {
+    // Obtenemos el controlador que queremos cargar
+    $controller = $_REQUEST['c'];
+    $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
+    
+    // Instanciamos el controlador
+    $controller = new controller;
+    
+    // Llama la accion
+    call_user_func( array( $controller, $accion ) );
+}
