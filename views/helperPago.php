8d3cdccc36a45a0b00db920636bb0028849204d7
Jth Rdz <fernetsystem@gmail.com>
2015-12-13 23:32:46 -0600

    mostrar helper de pago

---
+++ views/helperPago.php
@@ -0,0 +1,11 @@
+<br/>
+<div class="container">
+	<div class="panel panel-primary">
+  		<div class="panel-heading">INFORMACION SOBRE TU PAGO</div>
+  		<div class="panel-body">
+    		<h3>SE REALIZO TU TRANSACCION CORRECTAMENTE POR $12287.00</h3>
+    		<p>Gracias por el contrato de nuestros servicios...</p>
+    		<p>La cuenta se activara despues de que el pago sea verifiado por los administradores del sistema</p>
+		</div>
+	</div>
+</div>
