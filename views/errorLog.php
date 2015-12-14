f8b40fa0783f01a6c414b646d9c7bc5b0f0cbd9d
Jth Rdz <fernetsystem@gmail.com>
2015-12-13 23:25:29 -0600

    helpers

---
+++ views/errorLog.php
@@ -0,0 +1,9 @@
+<br/>
+<div class="container">
+	<div class="panel panel-danger">
+  		<div class="panel-heading">ERROR</div>
+  		<div class="panel-body">
+    		<h3>OCURRIO UN ERROR AL REALIZAR LA OPERACION SOLICITADA</h3>
+		</div>
+	</div>
+</div>
---
+++ views/successLog.php
@@ -0,0 +1,9 @@
+<br/>
+<div class="container">
+	<div class="panel panel-success">
+  		<div class="panel-heading">OPERACIÓN EXITOSA</div>
+  		<div class="panel-body">
+    		<p>LA OPERACIÓN QUE REALIZO SE HA EJECUTADO CORRECTAMENTE</p>
+		</div>
+	</div>
+</div>
